10dfee1f0966c6289ac23ecd7e6a07a98a98b94f
Jacob Berman <jacobberman1995@gmail.com>
2018-08-08 02:21:35 +0000

    automatically backed up by learn

--- lib/intro_to_arrays.rb
+++ lib/intro_to_arrays.rb
@@ -1,4 +1,15 @@
 def instantiate_new_array
   []
 end 
-def 
+
+def array_with_two_elements
+  [1, 2]
+end 
+
+def first_element(array)
+  array.first 
+end 
+
+def third_element(array)
+  array[2]
+end
